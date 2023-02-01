3864c1129a42c543561128b14760f4afe551a9d0
Aisen60 <caixusheng60@gmail.com>
2023-02-01 23:58:50 +0800

    feat: complete store state in urls

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import { useState, useEffect } from "react";
+
+import { Editor } from "@bytemd/react";
 import frontmatter from "@bytemd/plugin-frontmatter";
 import gemoji from "@bytemd/plugin-gemoji";
 import gfm from "@bytemd/plugin-gfm";
@@ -5,90 +8,152 @@ import highlight from "@bytemd/plugin-highlight";
 import math from "@bytemd/plugin-math";
 import mediumZoom from "@bytemd/plugin-medium-zoom";
 import mermaid from "@bytemd/plugin-mermaid";
-import { Editor } from "@bytemd/react";
+
+import mdLocales from "bytemd/locales/zh_Hans.json";
+import gfmLocales from "@bytemd/plugin-gfm/locales/zh_Hans.json";
+import mathLocales from "@bytemd/plugin-math/locales/zh_Hans.json";
+import mermaidLocales from "@bytemd/plugin-mermaid/locales/zh_Hans.json";
+
 import "bytemd/dist/index.css";
 import "github-markdown-css";
 import "highlight.js/styles/vs.css";
-// placed after highlight styles to override `code` padding
 import "katex/dist/katex.css";
-import { useState } from "react";
 
-const localeKey = "zh_Hans";
+import Share from "./components/icon/Share";
+import GitHub from "./components/icon/Github";
+import Download from "./components/icon/Download";
 
-function stripPrefixes(obj: Record<string, any>) {
-  return Object.entries(obj).reduce((p, [key, value]) => {
-    p[key.split("/").slice(-1)[0].replace(".json", "")] = value;
-    // console.log(p)
-    return p;
-  }, {} as Record<string, any>);
-}
+import { debounce, utoa, atou } from "./utils";
 
-const locales = stripPrefixes(
-  import.meta.glob("/node_modules/bytemd/locales/*.json", { eager: true })
-);
-
-const gfmLocales = stripPrefixes(
-  import.meta.glob("/node_modules/@bytemd/plugin-gfm/locales/*.json", {
-    eager: true,
-  })
-);
-
-const mathLocales = stripPrefixes(
-  import.meta.glob("/node_modules/@bytemd/plugin-math/locales/*.json", {
-    eager: true,
-  })
-);
-
-const mermaidLocales = stripPrefixes(
-  import.meta.glob("/node_modules/@bytemd/plugin-mermaid/locales/*.json", {
-    eager: true,
-  })
-);
+type AppState = {
+  time: number;
+  data: string;
+};
 
 const plugins = [
   frontmatter(),
   gemoji(),
   gfm({
-    locale: gfmLocales[localeKey],
+    locale: gfmLocales,
   }),
   highlight(),
   math({
-    locale: mathLocales[localeKey],
-    katexOptions: { output: "html" }, // https://github.com/KaTeX/KaTeX/issues/2796
+    locale: mathLocales,
+    katexOptions: { output: "html" },
   }),
   mediumZoom(),
   mermaid({
-    locale: mermaidLocales[localeKey],
+    locale: mermaidLocales,
   }),
 ].filter((x) => x);
 
-function App() {
-  const [value, setValue] = useState("");
-  const mode = "split";
+const Header = () => {
+  const copyLink = async () => {
+    await navigator.clipboard.writeText(location.href);
+    alert("可共享的URL已复制到剪贴板。");
+  };
+
+  const downloadFile = () => {
+    alert("下载还没搞好");
+  };
 
   return (
-    <div className="container pt-4 pb-4 mx-auto">
+    <nav className="h-10 pl-4 pr-4 flex items-center justify-between">
+      <h1 className="text-xl">Markdown Playground</h1>
+      <div className="flex items-center">
+        <button
+          className="pr-4 text-gray-500 hover:text-gray-900"
+          title="复制URL"
+          onClick={copyLink}
+        >
+          <Share className="w-5 h-5" />
+        </button>
+        <button
+          className="pr-4 text-gray-500 hover:text-gray-900"
+          title="下载文件"
+          onClick={downloadFile}
+        >
+          <Download className="w-5 h-6" />
+        </button>
+        <button
+          className="text-gray-500 hover:text-gray-900"
+          title="查看 GitHub"
+        >
+          <a href="https://github.com/Aisen60/md" target="_blank">
+            <GitHub className="w-6 h-6" />
+          </a>
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+function App() {
+  const [value, setValue] = useState("");
+
+  const init = () => {
+    initValue();
+    initView();
+  };
+
+  const initValue = () => {
+    const hash = location.hash.slice(1);
+    if (hash) {
+      try {
+        const appState: AppState = JSON.parse(atou(hash));
+        setValue(appState.data);
+      } catch {
+        history.replaceState({}, "", "#");
+      }
+    } else {
+      history.replaceState({}, "", "#");
+    }
+  };
+
+  const initView = () => {
+    const el = document.querySelector(
+      `.bytemd-toolbar-icon.bytemd-tippy.bytemd-tippy-right[bytemd-tippy-path="5"]`
+    ) as HTMLElement;
+    el.style.display = "none";
+  };
+
+  const onChange = debounce((data: string) => {
+    setValue(data);
+    if (data.trim()) {
+      const appState: AppState = { time: new Date().getTime(), data };
+      history.replaceState({}, "", "#" + utoa(JSON.stringify(appState)));
+    } else {
+      history.replaceState({}, "", "#");
+    }
+  }, 250);
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  return (
+    <div>
+      <Header />
       <Editor
         value={value}
-        mode={mode}
+        mode="split"
         plugins={plugins}
         placeholder={"Start writing"}
-        locale={locales[localeKey]}
+        locale={mdLocales}
         uploadImages={(files) => {
-          return Promise.all(
+          alert("图片上传还没搞好");
+          return Promise.reject(
             files.map((file) => {
               // TODO:
               return {
-                alt:'图片上传还没搞好',
-                title:'图片上传还没搞好',
-                url: "https://caixusheng.com/favicon.ico",
+                alt: "图片上传还没搞好",
+                title: "图片上传还没搞好",
+                // url: "https://caixusheng.com/favicon.ico",
               };
             })
           );
         }}
-        onChange={(e) => {
-          setValue(e);
-        }}
+        onChange={onChange}
       />
     </div>
   );
---
+++ src/components/icon/Share.tsx
@@ -0,0 +1,26 @@
+type Props = {
+  className: string;
+};
+
+const Share: React.FC<Props> = (props) => {
+  const { className } = props;
+  return (
+    <svg className={className} width="1.4em" height="1.4em" viewBox="0 0 24 24">
+      <g
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <circle cx="18" cy="5" r="3" />
+        <circle cx="6" cy="12" r="3" />
+        <circle cx="18" cy="19" r="3" />
+        <path d="M8.59 13.51l6.83 3.98" />
+        <path d="M15.41 6.51l-6.82 3.98" />
+      </g>
+    </svg>
+  );
+};
+
+export default Share;
